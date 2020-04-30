381b05ea728b363dda5aeba2a0c964678bebe558
ranjit0027 <ranjit0027@gmail.com>
2020-05-01 01:15:22 +0530

    update for kafka implementation
    update for kafka implementation

--- Wisors_WS/Wisors/src/main/java/com/wisors/registration/kafka/TopicProducer.java
+++ Wisors_WS/Wisors/src/main/java/com/wisors/registration/kafka/TopicProducer.java
@@ -33,9 +33,50 @@ public class TopicProducer {
 	@Value("${user.registration.kafka.topic.name}")
 	private String REGISTRATION_TOPIC;
 
-	@Value("${user.registration.response.kafka.topic.name}")
-	private String REGISTRATION_RESPONSE_TOPIC;
+	//@Value("${user.registration.response.kafka.topic.name}")
+	//private String REGISTRATION_RESPONSE_TOPIC;
 
+	
+	@Value("${createuser.client.id}")
+	private String CREATE_USER_ID;
+	
+	@Value("${updateuser.client.id}")
+	private String UPDATE_USER_ID;
+	
+	@Value("${retriveuser.client.id}")
+	private String RETRIVE_USER_ID;
+	
+	@Value("${deleteuser.client.id}")
+	private String DELETE_USER_ID;
+	
+	@Value("${error.client.id}")
+	private String ERROR_ID;
+	
+	@Value("${error2.client.id}")
+	private String ERROR2_ID;
+	
+	@Value("${retriveAllUser.client.id}")
+	private String RETRIVE_ALL_USER_ID;
+	
+	
+	@Value("${createuser.response.client.id}")
+	private String CREATE_RESPONSE_USER_ID;
+	
+	@Value("${updateuser.response.client.id}")
+	private String UPDATE_RESPONSE_USER_ID;
+	
+	@Value("${retriveuser.response.client.id}")
+	private String RETRIVE_RESPONSE_USER_ID;
+	
+	@Value("${deleteuser.response.client.id}")
+	private String DELETE_RESPONSE_USER_ID;
+	
+	
+	@Value("${retriveAllUser.response.client.id}")
+	private String RETRIVE_ALL_USER_RESPONSE_USER_ID;
+	
+		
+	
 	@Autowired
 	private KafkaTemplate<String, UserInfo> createUserKafkaTemplate;
 
@@ -79,28 +120,28 @@ public class TopicProducer {
 
 	public void sendCreateUserMessage(UserInfo userInfo) {
 		log.info("Recived create message : " + userInfo.toString());
-		this.createUserKafkaTemplate.send(REGISTRATION_TOPIC, "createUser", userInfo);
+		this.createUserKafkaTemplate.send(REGISTRATION_TOPIC, CREATE_USER_ID, userInfo);
 	}
 
 	public void sendUpdatUeserMessage(UserInfo userinfo, String phoneNo) {
 		log.info("Recived update message: " + userinfo.toString() + " , " + phoneNo);
-		this.updateUserKafkaTemplate.send(REGISTRATION_TOPIC, "updateUser", userinfo);
+		this.updateUserKafkaTemplate.send(REGISTRATION_TOPIC, UPDATE_USER_ID, userinfo);
 	}
 
 	public void sendRetriveUeserMessage(String phoneno) {
 		log.info("Recived search message : " + phoneno);
-		this.retriveUserKafkaTemplate.send(REGISTRATION_TOPIC, "retriveUser", String.valueOf(phoneno));
+		this.retriveUserKafkaTemplate.send(REGISTRATION_TOPIC, RETRIVE_USER_ID, String.valueOf(phoneno));
 	}
 
 	public void sendDeleteUeserMessage(String phoneno) {
 
 		log.info("Received delete message : " + phoneno);
-		this.deleteUserKafkaTemplate.send(REGISTRATION_TOPIC, "deleteUser", String.valueOf(phoneno));
+		this.deleteUserKafkaTemplate.send(REGISTRATION_TOPIC, DELETE_USER_ID, String.valueOf(phoneno));
 	}
 
 	public void sendRetriveAllUeserMessage() {
 		log.info("Received Retrive ALL  message :");
-		this.retriveAllUserKafkaTemplate.send(REGISTRATION_TOPIC, "retriveAllUser", 0);
+		this.retriveAllUserKafkaTemplate.send(REGISTRATION_TOPIC, RETRIVE_ALL_USER_ID, 0);
 	}
 
 	public void sendCreateUserAccountResponseMessage(WsrUserAccount wsrUserAct, RegistrationError error,
@@ -110,9 +151,9 @@ public class TopicProducer {
 		log.info("Recived responce create errorMsg  ==> : " + errorMsg);
 
 		if (wsrUserAct != null && error == null && errorMsg == null) {
-			this.createresponseKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "createUser", wsrUserAct);
+			this.createresponseKafkaTemplate.send(REGISTRATION_TOPIC, CREATE_RESPONSE_USER_ID, wsrUserAct);
 		} else if (wsrUserAct == null && error != null && errorMsg == null) {
-			this.errorKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "error", error);
+			this.errorKafkaTemplate.send(REGISTRATION_TOPIC, ERROR_ID, error);
 		}
 	}
 
@@ -121,9 +162,9 @@ public class TopicProducer {
 		log.info("Recived responce update message ==> : " + wsrUserAct);
 
 		if (wsrUserAct != null && error == null && errorMsg == null) {
-			this.updateresponseKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "updateUser", wsrUserAct);
+			this.updateresponseKafkaTemplate.send(REGISTRATION_TOPIC, UPDATE_RESPONSE_USER_ID , wsrUserAct);
 		} else if (wsrUserAct == null && error != null && errorMsg == null) {
-			this.errorKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "error", error);
+			this.errorKafkaTemplate.send(REGISTRATION_TOPIC, ERROR_ID, error);
 		}
 	}
 
@@ -134,24 +175,24 @@ public class TopicProducer {
 		log.info("Recived responce retrive error ==> : " + error);
 
 		if (wsrUserAct != null && error == null && errorMsg == null) {
-			this.retriveresponseKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "retriveUser", wsrUserAct);
+			this.retriveresponseKafkaTemplate.send(REGISTRATION_TOPIC, RETRIVE_RESPONSE_USER_ID, wsrUserAct);
 		} else if (wsrUserAct == null && error != null && errorMsg == null) {
-			this.errorKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "error", error);
+			this.errorKafkaTemplate.send(REGISTRATION_TOPIC, ERROR_ID, error);
 		} else {
-			this.errorKafkaTemplate2.send(REGISTRATION_RESPONSE_TOPIC, "error2", errorMsg);
+			this.errorKafkaTemplate2.send(REGISTRATION_TOPIC, ERROR2_ID, errorMsg);
 		}
 
 	}
 
 	public void sendDeleteUserAccountResponseMessage(String str, HttpStatus ok) {
 		log.info("Recived response delete message ==> : " + HttpStatus.OK);
-		this.errorKafkaTemplate2.send(REGISTRATION_RESPONSE_TOPIC, "deleteUser", str);
+		this.errorKafkaTemplate2.send(REGISTRATION_TOPIC, DELETE_RESPONSE_USER_ID, str);
 
 	}
 
 	public void sendRetriveAllUserAccountResponseMessage(List<WsrUserAccount> list) {
 		log.info("Recived response retrive all  message ==> : " + list);
-		this.retriveAllResponseKafkaTemplate.send(REGISTRATION_RESPONSE_TOPIC, "retriveAllUser", list);
+		this.retriveAllResponseKafkaTemplate.send(REGISTRATION_TOPIC, RETRIVE_ALL_USER_RESPONSE_USER_ID, list);
 
 	}
 
